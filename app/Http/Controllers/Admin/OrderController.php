a6db344f0a1fd36d871965629dfc18dfad5fce9e
MBABAZI Jacques <mbabazijacques@gmail.com>
2022-08-29 15:53:20 +0200

    added pagination on orders

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\MassDestroyOrderRequest;
+use App\Http\Requests\StoreOrderRequest;
 use App\Http\Requests\UpdateOrderRequest;
 use App\Models\Order;
 use App\Models\User;
 use Gate;
+use App\Notifications\OrderUpdatedNotification;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Yajra\DataTables\Facades\DataTables;
@@ -17,53 +20,9 @@ class OrderController extends Controller
     {
         abort_if(Gate::denies('order_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
-        if ($request->ajax()) {
-            $query = Order::with(['updated_by'])->select(sprintf('%s.*', (new Order())->table));
-            $table = Datatables::of($query);
+        $orders = Order::with(['updated_by'])->get();
 
-            $table->addColumn('placeholder', '&nbsp;');
-            $table->addColumn('actions', '&nbsp;');
-
-            $table->editColumn('actions', function ($row) {
-                $viewGate = 'order_show';
-                $editGate = 'order_edit';
-                $deleteGate = 'order_delete';
-                $crudRoutePart = 'orders';
-
-                return view('partials.datatablesActions', compact(
-                'viewGate',
-                'editGate',
-                'deleteGate',
-                'crudRoutePart',
-                'row'
-            ));
-            });
-
-            $table->editColumn('id', function ($row) {
-                return $row->id ? $row->id : '';
-            });
-            $table->editColumn('order_no', function ($row) {
-                return $row->order_no ? $row->order_no : '';
-            });
-            $table->editColumn('client_name', function ($row) {
-                return $row->client_name ? $row->client_name : '';
-            });
-            $table->editColumn('client_phone', function ($row) {
-                return $row->client_phone ? $row->client_phone : '';
-            });
-            $table->editColumn('shipping_address', function ($row) {
-                return $row->shipping_address ? $row->shipping_address : '';
-            });
-            $table->editColumn('status', function ($row) {
-                return $row->status ? Order::STATUS_SELECT[$row->status] : '';
-            });
-
-            $table->rawColumns(['actions', 'placeholder']);
-
-            return $table->make(true);
-        }
-
-        return view('admin.orders.index');
+        return view('admin.orders.index',compact('orders'));
     }
 
     public function edit(Order $order)
