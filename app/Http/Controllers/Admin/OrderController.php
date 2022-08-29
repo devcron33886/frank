0fb7f50d2b321743de7e4886d21d0a30ec28c6f4
MBABAZI Jacques <mbabazijacques@gmail.com>
2022-08-29 15:42:34 +0200

    added pagination on orders

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -3,27 +3,68 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-
 use App\Http\Requests\UpdateOrderRequest;
-use App\Notifications\OrderUpdatedNotification;
 use App\Models\Order;
 use App\Models\User;
 use Gate;
+use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Yajra\DataTables\Facades\DataTables;
 
 class OrderController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         abort_if(Gate::denies('order_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
-        $orders = Order::with(['updated_by'])
-        ->get();
+        if ($request->ajax()) {
+            $query = Order::with(['updated_by'])->select(sprintf('%s.*', (new Order())->table));
+            $table = Datatables::of($query);
 
-        return view('admin.orders.index', compact('orders'));
+            $table->addColumn('placeholder', '&nbsp;');
+            $table->addColumn('actions', '&nbsp;');
+
+            $table->editColumn('actions', function ($row) {
+                $viewGate = 'order_show';
+                $editGate = 'order_edit';
+                $deleteGate = 'order_delete';
+                $crudRoutePart = 'orders';
+
+                return view('partials.datatablesActions', compact(
+                'viewGate',
+                'editGate',
+                'deleteGate',
+                'crudRoutePart',
+                'row'
+            ));
+            });
+
+            $table->editColumn('id', function ($row) {
+                return $row->id ? $row->id : '';
+            });
+            $table->editColumn('order_no', function ($row) {
+                return $row->order_no ? $row->order_no : '';
+            });
+            $table->editColumn('client_name', function ($row) {
+                return $row->client_name ? $row->client_name : '';
+            });
+            $table->editColumn('client_phone', function ($row) {
+                return $row->client_phone ? $row->client_phone : '';
+            });
+            $table->editColumn('shipping_address', function ($row) {
+                return $row->shipping_address ? $row->shipping_address : '';
+            });
+            $table->editColumn('status', function ($row) {
+                return $row->status ? Order::STATUS_SELECT[$row->status] : '';
+            });
+
+            $table->rawColumns(['actions', 'placeholder']);
+
+            return $table->make(true);
+        }
+
+        return view('admin.orders.index');
     }
-
-    
 
     public function edit(Order $order)
     {
--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -1,137 +1,51 @@
 @extends('layouts.admin')
 @section('content')
-    <div class="content-wrapper" style="min-height: 706px;">
-        <!-- Content Header (Page header) -->
-        <div class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1 class="m-0 text-dark">Dashboard</h1>
-                    </div><!-- /.col -->
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="{{ route('admin.home') }}">Home</a></li>
-                            <li class="breadcrumb-item active">Orders</li>
-                        </ol>
-                    </div><!-- /.col -->
-                </div><!-- /.row -->
-            </div><!-- /.container-fluid -->
-        </div>
-        <section class="content">
-            <div class="container-fluid">
-                <div class="card">
-                    <div class="card-header">
-                        {{ trans('cruds.order.title_singular') }} {{ trans('global.list') }}
-                    </div>
-
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table class=" table table-bordered table-striped table-hover datatable datatable-Order">
-                                <thead>
-                                    <tr>
-                                        <th width="10">
-
-                                        </th>
-                                        <th>
-                                            {{ trans('cruds.order.fields.id') }}
-                                        </th>
-                                        <th>
-                                            {{ trans('cruds.order.fields.order_no') }}
-                                        </th>
-                                        <th>
-                                            {{ trans('cruds.order.fields.client_name') }}
-                                        </th>
-                                        <th>
-                                            {{ trans('cruds.order.fields.client_phone') }}
-                                        </th>
-
-                                        <th>
-                                            {{ trans('cruds.order.fields.status') }}
-                                        </th>
-
-
-                                        <th>
-                                            {{ trans('cruds.order.fields.payment_type') }}
-                                        </th>
-                                        <th>
-                                            &nbsp;
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($orders as $key => $order)
-                                        <tr data-entry-id="{{ $order->id }}">
-                                            <td>
-
-                                            </td>
-                                            <td>
-                                                {{ $order->id ?? '' }}
-                                            </td>
-                                            <td>
-                                                {{ $order->order_no ?? '' }}
-                                            </td>
-                                            <td>
-                                                {{ $order->client_name ?? '' }}
-                                            </td>
-                                            <td>
-                                                {{ $order->client_phone ?? '' }}
-                                            </td>
-
-                                            <td>
-                                                @if ($order->status == 'Pending')
-                                                    <span class="badge badge-warning">{{ $order->status }}</span>
-                                                @elseif($order->status == 'Processing')
-                                                    <span class="badge badge-info">{{ $order->status }}</span>
-                                                @elseif($order->status == 'Delivered')
-                                                    <span class="badge badge-success">{{ $order->status }}</span>
-                                                @elseif($order->status == 'Cancelled')
-                                                    <span class="badge badge-danger">{{ $order->status }}</span>
-                                                @elseif($order->status == 'On Way')
-                                                    <span class="badge badge-primary">{{ $order->status }}</span>
-                                                @endif
-                                            </td>
-
-                                            <td>
-                                                {{ $order->payment_type ?? '' }}
-                                            </td>
-                                            <td>
-                                                @can('order_show')
-                                                    <a class="btn btn-xs btn-primary"
-                                                        href="{{ route('admin.orders.show', $order->id) }}">
-                                                        {{ trans('global.view') }}
-                                                    </a>
-                                                @endcan
-
-                                                @can('order_edit')
-                                                    <a class="btn btn-xs btn-info"
-                                                        href="{{ route('admin.orders.edit', $order->id) }}">
-                                                        {{ trans('global.edit') }}
-                                                    </a>
-                                                @endcan
-
-                                                @can('order_delete')
-                                                    <form action="{{ route('admin.orders.destroy', $order->id) }}"
-                                                        method="POST"
-                                                        onsubmit="return confirm('{{ trans('global.areYouSure') }}');"
-                                                        style="display: inline-block;">
-                                                        <input type="hidden" name="_method" value="DELETE">
-                                                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                                        <input type="submit" class="btn btn-xs btn-danger"
-                                                            value="{{ trans('global.delete') }}">
-                                                    </form>
-                                                @endcan
-
-                                            </td>
-
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
+    @can('order_create')
+        <div style="margin-bottom: 10px;" class="row">
+            <div class="col-lg-12">
+                <a class="btn btn-success" href="{{ route('admin.orders.create') }}">
+                    {{ trans('global.add') }} {{ trans('cruds.order.title_singular') }}
+                </a>
             </div>
-        </section>
+        </div>
+    @endcan
+    <div class="card">
+        <div class="card-header">
+            {{ trans('cruds.order.title_singular') }} {{ trans('global.list') }}
+        </div>
+
+        <div class="card-body">
+            <table class=" table table-bordered table-striped table-hover ajaxTable datatable datatable-Order">
+                <thead>
+                    <tr>
+                        <th width="10">
+
+                        </th>
+                        <th>
+                            {{ trans('cruds.order.fields.id') }}
+                        </th>
+                        <th>
+                            {{ trans('cruds.order.fields.order_no') }}
+                        </th>
+                        <th>
+                            {{ trans('cruds.order.fields.client_name') }}
+                        </th>
+                        <th>
+                            {{ trans('cruds.order.fields.client_phone') }}
+                        </th>
+                        <th>
+                            {{ trans('cruds.order.fields.shipping_address') }}
+                        </th>
+                        <th>
+                            {{ trans('cruds.order.fields.status') }}
+                        </th>
+                        <th>
+                            &nbsp;
+                        </th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
     </div>
 @endsection
 @section('scripts')
@@ -140,7 +54,7 @@
         $(function() {
             let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons)
             @can('order_delete')
-                let deleteButtonTrans = '{{ trans('global.datatables.delete') }}'
+                let deleteButtonTrans = '{{ trans('global.datatables.delete') }}';
                 let deleteButton = {
                     text: deleteButtonTrans,
                     url: "{{ route('admin.orders.massDestroy') }}",
@@ -148,8 +62,8 @@
                     action: function(e, dt, node, config) {
                         var ids = $.map(dt.rows({
                             selected: true
-                        }).nodes(), function(entry) {
-                            return $(entry).data('entry-id')
+                        }).data(), function(entry) {
+                            return entry.id
                         });
 
                         if (ids.length === 0) {
@@ -179,21 +93,58 @@
                 dtButtons.push(deleteButton)
             @endcan
 
-            $.extend(true, $.fn.dataTable.defaults, {
+            let dtOverrideGlobals = {
+                buttons: dtButtons,
+                processing: true,
+                serverSide: true,
+                retrieve: true,
+                aaSorting: [],
+                ajax: "{{ route('admin.orders.index') }}",
+                columns: [{
+                        data: 'placeholder',
+                        name: 'placeholder'
+                    },
+                    {
+                        data: 'id',
+                        name: 'id'
+                    },
+                    {
+                        data: 'order_no',
+                        name: 'order_no'
+                    },
+                    {
+                        data: 'client_name',
+                        name: 'client_name'
+                    },
+                    {
+                        data: 'client_phone',
+                        name: 'client_phone'
+                    },
+                    {
+                        data: 'shipping_address',
+                        name: 'shipping_address'
+                    },
+                    {
+                        data: 'status',
+                        name: 'status'
+                    },
+                    {
+                        data: 'actions',
+                        name: '{{ trans('global.actions') }}'
+                    }
+                ],
                 orderCellsTop: true,
                 order: [
                     [1, 'desc']
                 ],
                 pageLength: 10,
-            });
-            let table = $('.datatable-Order:not(.ajaxTable)').DataTable({
-                buttons: dtButtons
-            })
+            };
+            let table = $('.datatable-Order').DataTable(dtOverrideGlobals);
             $('a[data-toggle="tab"]').on('shown.bs.tab click', function(e) {
                 $($.fn.dataTable.tables(true)).DataTable()
                     .columns.adjust();
             });
 
-        })
+        });
     </script>
 @endsection
