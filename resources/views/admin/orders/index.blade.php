2e4d087f7579601667228a0a6e383ac6c4c572a6
MBABAZI Jacques <mbabazijacques@gmail.com>
2022-08-30 13:58:35 +0200

    fixed dashboard numbers

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -1,119 +1,163 @@
 @extends('layouts.admin')
 @section('content')
-@can('order_create')
-    <div style="margin-bottom: 10px;" class="row">
-        <div class="col-lg-12">
-            <a class="btn btn-success" href="{{ route('admin.orders.create') }}">
-                {{ trans('global.add') }} {{ trans('cruds.order.title_singular') }}
-            </a>
+    <!-- Content Header (Page header) -->
+    <div class="content-wrapper" style="min-height: 706px;">
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0 text-dark">Dashboard</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="{{ route('admin.home') }}">Home</a></li>
+                            <li class="breadcrumb-item active">Orders</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
         </div>
+        <section class="content">
+            <div class="container-fluid">
+                <div class="card">
+                    <div class="card-header">
+                        {{ trans('cruds.order.title_singular') }} {{ trans('global.list') }}
+                    </div>
+
+                    <div class="card-body">
+                        <table class=" table table-bordered table-striped table-hover ajaxTable datatable datatable-Order">
+                            <thead>
+                                <tr>
+                                    <th width="10">
+
+                                    </th>
+                                    <th>
+                                        Id
+                                    </th>
+                                    <th>
+                                        Order No
+                                    </th>
+                                    <th>
+                                        Client Name
+                                    </th>
+                                    <th>
+                                        Client Phone
+                                    </th>
+                                    <th>
+                                        Shipping Address
+                                    </th>
+                                    <th>
+                                        Status
+                                    </th>
+                                    <th>
+                                        &nbsp;
+                                    </th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
-@endcan
-<div class="card">
-    <div class="card-header">
-        {{ trans('cruds.order.title_singular') }} {{ trans('global.list') }}
-    </div>
+    @endsection
+    @section('scripts')
+        @parent
+        <script>
+            $(function() {
+                let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons)
+                @can('order_delete')
+                    let deleteButtonTrans = '{{ trans('global.datatables.delete') }}';
+                    let deleteButton = {
+                        text: deleteButtonTrans,
+                        url: "{{ route('admin.orders.massDestroy') }}",
+                        className: 'btn-danger',
+                        action: function(e, dt, node, config) {
+                            var ids = $.map(dt.rows({
+                                selected: true
+                            }).data(), function(entry) {
+                                return entry.id
+                            });
 
-    <div class="card-body">
-        <table class=" table table-bordered table-striped table-hover ajaxTable datatable datatable-Order">
-            <thead>
-                <tr>
-                    <th width="10">
+                            if (ids.length === 0) {
+                                alert('{{ trans('global.datatables.zero_selected') }}')
 
-                    </th>
-                    <th>
-                        {{ trans('cruds.order.fields.id') }}
-                    </th>
-                    <th>
-                        {{ trans('cruds.order.fields.order_no') }}
-                    </th>
-                    <th>
-                        {{ trans('cruds.order.fields.client_name') }}
-                    </th>
-                    <th>
-                        {{ trans('cruds.order.fields.client_phone') }}
-                    </th>
-                    <th>
-                        {{ trans('cruds.order.fields.shipping_address') }}
-                    </th>
-                    <th>
-                        {{ trans('cruds.order.fields.status') }}
-                    </th>
-                    <th>
-                        &nbsp;
-                    </th>
-                </tr>
-            </thead>
-        </table>
-    </div>
-</div>
+                                return
+                            }
 
+                            if (confirm('{{ trans('global.areYouSure') }}')) {
+                                $.ajax({
+                                        headers: {
+                                            'x-csrf-token': _token
+                                        },
+                                        method: 'POST',
+                                        url: config.url,
+                                        data: {
+                                            ids: ids,
+                                            _method: 'DELETE'
+                                        }
+                                    })
+                                    .done(function() {
+                                        location.reload()
+                                    })
+                            }
+                        }
+                    }
+                    dtButtons.push(deleteButton)
+                @endcan
 
+                let dtOverrideGlobals = {
+                    buttons: dtButtons,
+                    processing: true,
+                    serverSide: true,
+                    retrieve: true,
+                    aaSorting: [],
+                    ajax: "{{ route('admin.orders.index') }}",
+                    columns: [{
+                            data: 'placeholder',
+                            name: 'placeholder'
+                        },
+                        {
+                            data: 'id',
+                            name: 'id'
+                        },
+                        {
+                            data: 'order_no',
+                            name: 'order_no'
+                        },
+                        {
+                            data: 'clientName',
+                            name: 'clientName'
+                        },
+                        {
+                            data: 'clientPhone',
+                            name: 'clientPhone'
+                        },
+                        {
+                            data: 'shipping_address',
+                            name: 'shipping_address'
+                        },
+                        {
+                            data: 'status',
+                            name: 'status'
+                        },
+                        {
+                            data: 'actions',
+                            name: '{{ trans('global.actions') }}'
+                        }
+                    ],
+                    orderCellsTop: true,
+                    order: [
+                        [1, 'desc']
+                    ],
+                    pageLength: 10,
+                };
+                let table = $('.datatable-Order').DataTable(dtOverrideGlobals);
+                $('a[data-toggle="tab"]').on('shown.bs.tab click', function(e) {
+                    $($.fn.dataTable.tables(true)).DataTable()
+                        .columns.adjust();
+                });
 
-@endsection
-@section('scripts')
-@parent
-<script>
-    $(function () {
-  let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons)
-@can('order_delete')
-  let deleteButtonTrans = '{{ trans('global.datatables.delete') }}';
-  let deleteButton = {
-    text: deleteButtonTrans,
-    url: "{{ route('admin.orders.massDestroy') }}",
-    className: 'btn-danger',
-    action: function (e, dt, node, config) {
-      var ids = $.map(dt.rows({ selected: true }).data(), function (entry) {
-          return entry.id
-      });
-
-      if (ids.length === 0) {
-        alert('{{ trans('global.datatables.zero_selected') }}')
-
-        return
-      }
-
-      if (confirm('{{ trans('global.areYouSure') }}')) {
-        $.ajax({
-          headers: {'x-csrf-token': _token},
-          method: 'POST',
-          url: config.url,
-          data: { ids: ids, _method: 'DELETE' }})
-          .done(function () { location.reload() })
-      }
-    }
-  }
-  dtButtons.push(deleteButton)
-@endcan
-
-  let dtOverrideGlobals = {
-    buttons: dtButtons,
-    processing: true,
-    serverSide: true,
-    retrieve: true,
-    aaSorting: [],
-    ajax: "{{ route('admin.orders.index') }}",
-    columns: [
-      { data: 'placeholder', name: 'placeholder' },
-{ data: 'id', name: 'id' },
-{ data: 'order_no', name: 'order_no' },
-{ data: 'client_name', name: 'client_name' },
-{ data: 'client_phone', name: 'client_phone' },
-{ data: 'shipping_address', name: 'shipping_address' },
-{ data: 'status', name: 'status' },
-{ data: 'actions', name: '{{ trans('global.actions') }}' }
-    ],
-    orderCellsTop: true,
-    order: [[ 1, 'desc' ]],
-    pageLength: 10,
-  };
-  let table = $('.datatable-Order').DataTable(dtOverrideGlobals);
-  $('a[data-toggle="tab"]').on('shown.bs.tab click', function(e){
-      $($.fn.dataTable.tables(true)).DataTable()
-          .columns.adjust();
-  });
-  
-});
-
-</script>
-@endsection
+            });
+        </script>
+    @endsection
