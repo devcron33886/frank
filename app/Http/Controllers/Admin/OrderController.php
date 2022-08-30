f6a6cd1aef27334fc774efe43509a2dc110666eb
MBABAZI Jacques <mbabazijacques@gmail.com>
2022-08-30 16:10:57 +0200

    orders pagination

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -10,61 +10,15 @@ use Gate;
 use App\Notifications\OrderUpdatedNotification;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Yajra\DataTables\Facades\DataTables;
 
 class OrderController extends Controller
 {
     public function index(Request $request)
     {
         abort_if(Gate::denies('order_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
-
-        if ($request->ajax()) {
-            $query = Order::with(['updated_by'])->select(sprintf('%s.*', (new Order())->table));
-            $table = Datatables::of($query);
-
-            $table->addColumn('placeholder', '&nbsp;');
-            $table->addColumn('actions', '&nbsp;');
-
-            $table->editColumn('actions', function ($row) {
-                $viewGate = 'order_show';
-                $editGate = 'order_edit';
-                $deleteGate = 'order_delete';
-                $crudRoutePart = 'orders';
-
-                return view('partials.datatablesActions', compact(
-                    'viewGate',
-                    'editGate',
-                    'deleteGate',
-                    'crudRoutePart',
-                    'row'
-                ));
-            });
-
-            $table->editColumn('id', function ($row) {
-                return $row->id ? $row->id : '';
-            });
-            $table->editColumn('order_no', function ($row) {
-                return $row->order_no ? $row->order_no : '';
-            });
-            $table->editColumn('clientName', function ($row) {
-                return $row->clientName ? $row->clientName : '';
-            });
-            $table->editColumn('clientPhone', function ($row) {
-                return $row->clientPhone ? $row->clientPhone : '';
-            });
-            $table->editColumn('shipping_address', function ($row) {
-                return $row->shipping_address ? $row->shipping_address : '';
-            });
-            $table->editColumn('status', function ($row) {
-                return $row->status ? Order::STATUS_SELECT[$row->status] : '';
-            });
-
-            $table->rawColumns(['actions', 'placeholder']);
-
-            return $table->make(true);
-        }
-
-        return view('admin.orders.index');
+        $orders=Order::orderBy('id','desc')->paginate(10);
+       
+        return view('admin.orders.index',compact('orders'));
     }
 
     public function edit(Order $order)
--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -25,12 +25,10 @@
                     </div>
 
                     <div class="card-body">
-                        <table class=" table table-bordered table-striped table-hover ajaxTable datatable datatable-Order">
+                        <table class=" table table-bordered table-striped table-hover" id="ordersTable">
                             <thead>
                                 <tr>
-                                    <th width="10">
-
-                                    </th>
+                                    
                                     <th>
                                         {{ trans('cruds.order.fields.id') }}
                                     </th>
@@ -54,7 +52,37 @@
                                     </th>
                                 </tr>
                             </thead>
+                            <tbody>
+                                @foreach($orders as $key => $order)
+                                    <tr data-entry-id="{{ $order->id }}">
+                                        
+                                        <td>
+                                            {{ $order->id ?? '' }}
+                                        </td>
+                                        <td>
+                                            {{ $order->order_no ?? '' }}
+                                        </td>
+                                        <td>
+                                            {{ $order->clientName ?? '' }}
+                                        </td>
+                                        <td>
+                                            {{ $order->clientPhone ?? '' }}
+                                        </td>
+                                        <td>
+                                            {{ $order->shipping_address ?? '' }}
+                                        </td>
+                                        <td>
+                                            {{ $order->status ?? '' }}
+                                        </td>
+                                        <td>
+                                            <a class="btn btn-xs btn-primary" href="{{ route('admin.orders.show', $order->id) }}">
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
                         </table>
+                        {{ $orders->links() }}
                     </div>
                 </div>
             </div>
@@ -64,100 +92,6 @@
     @section('scripts')
         @parent
         <script>
-            $(function() {
-                let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons)
-                @can('order_delete')
-                    let deleteButtonTrans = '{{ trans('global.datatables.delete') }}';
-                    let deleteButton = {
-                        text: deleteButtonTrans,
-                        url: "{{ route('admin.orders.massDestroy') }}",
-                        className: 'btn-danger',
-                        action: function(e, dt, node, config) {
-                            var ids = $.map(dt.rows({
-                                selected: true
-                            }).data(), function(entry) {
-                                return entry.id
-                            });
-
-                            if (ids.length === 0) {
-                                alert('{{ trans('global.datatables.zero_selected') }}')
-
-                                return
-                            }
-
-                            if (confirm('{{ trans('global.areYouSure') }}')) {
-                                $.ajax({
-                                        headers: {
-                                            'x-csrf-token': _token
-                                        },
-                                        method: 'POST',
-                                        url: config.url,
-                                        data: {
-                                            ids: ids,
-                                            _method: 'DELETE'
-                                        }
-                                    })
-                                    .done(function() {
-                                        location.reload()
-                                    })
-                            }
-                        }
-                    }
-                    dtButtons.push(deleteButton)
-                @endcan
-
-                let dtOverrideGlobals = {
-                    buttons: dtButtons,
-                    processing: true,
-                    serverSide: true,
-                    retrieve: true,
-                    aaSorting: [],
-                    ajax: "{{ route('admin.orders.index') }}",
-                    columns: [{
-                            data: 'placeholder',
-                            name: 'placeholder'
-                        },
-                        {
-                            data: 'id',
-                            name: 'id'
-                        },
-                        {
-                            data: 'order_no',
-                            name: 'order_no'
-                        },
-                        {
-                            data: 'clientName',
-                            name: 'clientName'
-                        },
-                        {
-                            data: 'clientPhone',
-                            name: 'clientPhone'
-                        },
-                        {
-                            data: 'shipping_address',
-                            name: 'shipping_address'
-                        },
-                        {
-                            data: 'status',
-                            name: 'status'
-                        },
-                        {
-                            data: 'actions',
-                            name: '{{ trans('global.actions') }}'
-                        }
-                    ],
-                    orderCellsTop: true,
-                    order: [
-                        [1, 'desc']
-                    ],
-                    pageLength: 10,
-                };
-                let table = $('.datatable-Order').DataTable(dtOverrideGlobals);
-                $('a[data-toggle="tab"]').on('shown.bs.tab click', function(e) {
-                    $($.fn.dataTable.tables(true)).DataTable()
-                        .columns.adjust();
-                });
-
-            });
+           
         </script>
     @endsection
